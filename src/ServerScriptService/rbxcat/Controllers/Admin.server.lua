d1e504274ddb417d86bbc87fdbdf982279d62597
fartg <catfoodbeer@gmail.com>
2024-01-30 04:37:51 -0500

    add a skipminigame function for admins :P

--- src/ServerScriptService/rbxcat/Controllers/Admin.server.lua
+++ src/ServerScriptService/rbxcat/Controllers/Admin.server.lua
@@ -8,3 +8,8 @@ end
 events.IsAdmin.OnServerInvoke = function(fired_player, player)
 	return Admin.Check(player);
 end
+
+events.SkipMinigame.OnServerEvent:Connect(function (fired_player)
+	if not Admin.Check(fired_player) then return end;
+	edit_server.Game("active", false);
+end)
